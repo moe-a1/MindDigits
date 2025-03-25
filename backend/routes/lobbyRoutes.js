f5c98725244414138721477de232512fcbe912ab
Mohamed Abdelfattah <moe.tarek6666@gmail.com>
2025-03-25 21:43:33 +0000

    return 200 status on GET & add .env to gitignore

--- backend/routes/lobbyRoutes.js
+++ backend/routes/lobbyRoutes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
       return res.status(404).json({ message: 'Lobby not found' });
     }
     
-    res.json(lobby);
+    res.status(200).json(lobby);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
