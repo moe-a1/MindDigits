24e38d12ae79113f707bbfa36593ef1f772f1d13
Mohamed Abdelfattah <moe.tarek6666@gmail.com>
2025-03-27 18:05:53 +0000

    push App js and css files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SocketProvider } from './context/SocketContext';
+import { GameProvider } from './context/GameContext';
+import Home from './pages/Home';
+import Lobby from './pages/Lobby';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SocketProvider>
+      <GameProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lobby/:lobbyId" element={<Lobby />} />
+          </Routes>
+        </Router>
+      </GameProvider>
+    </SocketProvider>
   );
 }
 
