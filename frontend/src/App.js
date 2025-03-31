d0c3b946cb940402eb6c7fc0611bab73709e6803
Mohamed Abdelfattah <moe.tarek6666@gmail.com>
2025-04-01 00:14:54 +0100

    move from pages folder to components folder for consistency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SocketProvider } from './context/SocketContext';
 import { GameProvider } from './context/GameContext';
-import Home from './pages/Home';
-import Lobby from './pages/Lobby';
-import Game from './pages/Game';
+import Home from './components/Home';
+import Lobby from './components/Lobby';
+import Game from './components/Game';
 import './App.css';
 
 function App() {
--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useGame } from '../context/GameContext';
-import DrawingCanvas from '../components/DrawingCanvas';
+import DrawingCanvas from './DrawingCanvas';
 import '../styles/Game.css';
 
 function Game() {
